182a3824b58151e75cb3cb94e22267b380056581
amwatah <jackalltrade2020@gmail.com>
2022-03-11 14:29:59 +0300

    finished project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [quote, setQuote] = useState();
+  const norrisImage =
+    "https://cdn.dribbble.com/users/382712/screenshots/4165164/media/8e5141aa7ee4f9efd6a1597f3e463ae6.png?compress=1&resize=400x300";
+  const year = new Date().getFullYear();
+  async function getQuote() {
+    const url = "http://api.icndb.com/jokes/random";
+    axios.get(url).then((response) => {
+      console.log(response.data.value.joke);
+      setQuote(response.data.value.joke);
+    });
+  }
+
+  useEffect(() => {
+    getQuote();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div className="page text-center p-1 flex flex-col  items-center relative h-[100vh] ">
+        <div className="image  rounded-md">
+          <img
+            src={norrisImage}
+            alt="badboy"
+            className="rounded-md w-[100vw] md:w-[50vw] "
+          />
+        </div>
+
+        <h1 className="qoute font-bold m-3 p-2">{quote}</h1>
+        <br />
+        <button
+          onClick={getQuote}
+          className=" bg-indigo-500 p-2 rounded-md uppercase  hover:bg-gradient-to-br hover:from-indigo-500 to hover:to-fuchsia-500 "
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          What else about Chuck ?
+        </button>
+        <footer className=" footer align-bottom absolute bottom-0  ">
+          @ {year} Amwatah Obuya
+        </footer>
+      </div>
+    </>
   );
 }
 
