925635f4faa3d65372b6465915636f8e134208eb
amwatah <jackalltrade2020@gmail.com>
2022-03-12 21:46:47 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,16 +4,31 @@ import "./App.css";
 
 function App() {
   const [quote, setQuote] = useState();
-  const url = "http://api.icndb.com/jokes/random";
+
   const norrisImage =
     "https://cdn.dribbble.com/users/382712/screenshots/4165164/media/8e5141aa7ee4f9efd6a1597f3e463ae6.png?compress=1&resize=400x300";
   const year = new Date().getFullYear();
+
   async function getQuote() {
-    
-    axios.get(url).then((response) => {
-      console.log(response.data.value.joke);
-      setQuote(response.data.value.joke);
-    });
+    const options = {
+      method: "GET",
+      url: "https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random",
+      headers: {
+        accept: "application/json",
+        "x-rapidapi-host": "matchilling-chuck-norris-jokes-v1.p.rapidapi.com",
+        "x-rapidapi-key": "fd0454f1b9msh9c01b195580c588p1f012bjsn7e6f631a737f",
+      },
+    };
+
+    axios
+      .request(options)
+      .then( (response)=> {
+         console.log( response.data);
+         setQuote( response.data)
+      })
+      .catch( (error)=> {
+        console.error(error);
+      });
   }
 
   useEffect(() => {
@@ -21,7 +36,7 @@ function App() {
   }, []);
   return (
     <>
-      <div className="page text-center p-1 flex flex-col  items-center relative h-[100vh] ">
+      <div className="page text-center p-1 flex flex-col   items-center relative h-screen ">
         <div className="image  rounded-md">
           <img
             src={norrisImage}
@@ -29,16 +44,18 @@ function App() {
             className="rounded-md w-[100vw] md:w-[50vw] "
           />
         </div>
+        <img src={quote.icon_url} alt="" />
 
-        <h1 className="qoute font-bold m-1 p-2">{quote}</h1>
-        <br />
+        <h1 className="qoute font-bold m-1 p-2">{quote.value}</h1>
         <button
           onClick={getQuote}
           className=" bg-indigo-500 p-2 rounded-md uppercase  hover:bg-gradient-to-br hover:from-indigo-500 to hover:to-fuchsia-500 "
         >
           What else about Chuck ?
         </button>
-        <footer className=" footer align-bottom absolute bottom-0  mt-1 ">
+        <br />
+       
+        <footer className=" footer align-bottom  bottom-0   ">
           @ {year} Amwatah Obuya
         </footer>
       </div>
